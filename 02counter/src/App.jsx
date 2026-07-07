ebe94ce07b0fc980d29fc2c5b9b761730eeae9c8
Jagriti-yadav <jagritiyadav1901@gmail.com>
2026-07-07 23:49:05 +0530

    hooks basics

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  let [counter,setCounter] = useState(12);
+
+  const addValue = () => {
+    setCounter(counter+1);
+    console.log("random value is : ", counter);
+  };
+
+  const removeValue=()=>{
+    setCounter(counter-1);
+  }
+  return (
+    <>
+      <h1>Counter App</h1>
+      <h2>count value: {counter}</h2>
+      <button onClick={addValue}>Add value</button>
+      <button onClick={removeValue}>Subtract value</button>
+    </>
+  );
+}
+
+export default App;
